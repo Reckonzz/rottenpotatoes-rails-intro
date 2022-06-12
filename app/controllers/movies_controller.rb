cb1286a836a11ab99da103229aa06870ff6fe698
Clarence Lee Sheng Sheng <1005266@mymail.sutd.edu.sg>
2022-06-12 03:20:20 +0000

    part 3 complete

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -8,18 +8,34 @@ class MoviesController < ApplicationController
 
   def index
     @all_ratings = Movie.all_ratings
+    
+
+    if !session.key?(:ratings) || !session.key?(:sort_by)
+      @all_ratings_hash = Hash[@all_ratings.collect {|key| [key, '1']}]
+      session[:ratings] = @all_ratings_hash if !session.key?(:ratings)
+      session[:sort_by] = '' if !session.key?(:sort_by)
+      redirect_to movies_path(:ratings => @all_ratings_hash, :sort_by => '') and return
+    end
+    
+    if !params.has_key?(:ratings) || !params.has_key?(:sort_by)
+      ratings = params.has_key?(:ratings) ? params[:ratings] : session[:ratings]
+      sort_by = params.has_key?(:sort_by) ? params[:sort_by] : session[:sort_by]
+      redirect_to movies_path(:ratings => ratings, :sort_by => sort_by) and return
+    end
+
     @ratings_to_show = params[:ratings] ? params[:ratings].keys : []
     @ratings_to_show_hash = Hash[@ratings_to_show.collect {|key| [key, '1']}]
-
     @movies = Movie.with_ratings(@ratings_to_show)
+    session[:ratings] = params[:ratings]
 
     column_selected_styles = "hilite bg-warning"
     column_classes = {@title_header=> "", @release_date_header=> ""}
 
     if params[:sort_by]
       instance_variable_set("@#{params[:sort_by]}_header", column_selected_styles)
-      @movies = @movies.order(params[:sort_by])
-    end 
+      @movies = params[:sort_by] ? @movies.order(params[:sort_by]) : @movies 
+      session[:sort_by] = params[:sort_by]
+    end     
   end
 
   def new
@@ -57,75 +73,3 @@ class MoviesController < ApplicationController
     params.require(:movie).permit(:title, :rating, :description, :release_date)
   end
 end
-
-
-# class MoviesController < ApplicationController
-
-#   def show
-#     id = params[:id] # retrieve movie ID from URI route
-#     @movie = Movie.find(id) # look up movie by unique ID
-#     # will render app/views/movies/show.<extension> by default
-#   end
-
-#   def index
-#     @all_ratings = Movie.all_ratings
-    
-#     if !session.key?(:ratings) || !session.key?(:sort_by)
-#       @all_ratings_hash = Hash[@all_ratings.collect {|key| [key, '1']}]
-#       session[:ratings] = @all_ratings_hash if !session.key?(:ratings)
-#       session[:sort_by] = '' if !session.key?(:sort_by)
-#       redirect_to movies_path(:ratings => @all_ratings_hash, :sort_by => '') and return
-#     end
-    
-#     if (!params.has_key?(:ratings) && session.key?(:ratings)) ||
-#       (!params.has_key?(:sort_by) && session.key?(:sort_by))
-#       redirect_to movies_path(:ratings => Hash[session[:ratings].collect {|key| [key, '1']}], :sort_by => session[:sort_by]) and return
-#     end
-    
-#     @ratings_to_show = params[:ratings].keys
-#     @ratings_to_show_hash = Hash[@ratings_to_show.collect {|key| [key, '1']}]
-#     session[:ratings] = @ratings_to_show
-    
-#     @movies = Movie.with_ratings(@ratings_to_show)
-    
-#     @movies = @movies.order(params[:sort_by]) if params[:sort_by] != ''
-#     session[:sort_by] = params[:sort_by]
-#     @title_header = (params[:sort_by]=='title') ? 'hilite bg-warning' : ''
-#     @release_date_header = (params[:sort_by]=='release_date') ? 'hilite bg-warning' : ''
-#   end
-
-#   def new
-#     # default: render 'new' template
-#   end
-
-#   def create
-#     @movie = Movie.create!(movie_params)
-#     flash[:notice] = "#{@movie.title} was successfully created."
-#     redirect_to movies_path
-#   end
-
-#   def edit
-#     @movie = Movie.find params[:id]
-#   end
-
-#   def update
-#     @movie = Movie.find params[:id]
-#     @movie.update_attributes!(movie_params)
-#     flash[:notice] = "#{@movie.title} was successfully updated."
-#     redirect_to movie_path(@movie)
-#   end
-
-#   def destroy
-#     @movie = Movie.find(params[:id])
-#     @movie.destroy
-#     flash[:notice] = "Movie '#{@movie.title}' deleted."
-#     redirect_to movies_path
-#   end
-
-#   private
-#   # Making "internal" methods private is not required, but is a common practice.
-#   # This helps make clear which methods respond to requests, and which ones do not.
-#   def movie_params
-#     params.require(:movie).permit(:title, :rating, :description, :release_date)
-#   end
-# end
